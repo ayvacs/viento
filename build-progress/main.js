9db8c7a5ecd0c25eeae62b3a4bd1f3b98d1099be
ave <ayvacs@proton.me>
2022-06-26 16:50:08 -0500

    wip

--- build-progress/main.js
+++ build-progress/main.js
@@ -8,7 +8,14 @@ var data = [
     ]
 ]
 
+var times = [];
+
 for (var i in data) {
     var v = data[i];
-    document.getElementById("data").innerHTML += `<tr><td>${v[1]}</td><td>${v[2]}</td><td>${v[3]}</td><td><img width=870px src="scr/${v[0]}.png"></td></tr>`;
+    var t = v[3]/60;
+
+    document.getElementById("data").innerHTML += `<tr><td>&nbsp;</td><td>${v[1]}</td><td>${v[2]}</td><td>${t}</td><td><img width=870px src="scr/${v[0]}.png"></td></tr>`;
+    times.push(t);
 };
+
+document.getElementById("data").innerHTML += `<tr><td><b>Sum</b></td><td></td><td></td><td><b>${times.reduce((partialSum, a) => partialSum + a, 0)}</b></td><td></td>`;
